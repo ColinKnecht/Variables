1c2c2882d4c707a6e7f942819e499c482469f10b
ColinKnecht <cknecht87@gmail.com>
2017-09-01 12:05:47 -0400

    Initial Commit
    Test Variables and Values with Xcode and Swift

---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var message = "Hello, playground"
+
+//operators
+//unary, binary, ternanry
+
+var isBoolean = true
+
+var bankAccountBalance = 100
+var cashRegisterMessage = bankAccountBalance >= 50 ? "U just bought it" : "You are broke"
